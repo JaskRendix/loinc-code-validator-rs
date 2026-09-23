2feb3dc706403858a3438e32da49a0dc327f9c9e
JaskRendix <tur3a.giorgio.menzi@gmail.com>
2026-09-23 16:36:12 +0200

    refactor: swap panic-driven unwraps for proper error bubbling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use loinc_code_validator_rs::app;
 
 #[tokio::main]
-async fn main() {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
 
     println!("Server running on http://127.0.0.1:3000");
 
-    axum::serve(listener, app()).await.unwrap();
+    axum::serve(listener, app()).await?;
+
+    Ok(())
 }
